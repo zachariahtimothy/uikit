87c2af802a3abea2c8032e8c0a1b2b2f9909f424
Zach Curtis <zachariahtimothy@gmail.com>
2015-05-11 08:47:40 -0600

    Fixed git URL

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'UIKit repackaged for Meteor (from fork)',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/zachariahtimothy/uikit',
+  git: 'git@github.com:MeteorPackaging/uikit.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
