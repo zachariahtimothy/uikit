570d7f00fdaff0c32b30958f9a952ddcdd6cc198
Zach Curtis <zachariahtimothy@gmail.com>
2016-01-22 11:40:15 -0700

    Pulled github back to mine

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'UIKit repackaged for Meteor (from fork)',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/MeteorPackaging/uikit',
+  git: 'https://github.com/zachariahtimothy/uikit',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
